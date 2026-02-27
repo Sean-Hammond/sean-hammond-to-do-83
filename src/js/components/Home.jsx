75e654880df5e898cb913b07bef0b04fc396f6fe
Sean W. Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-02-27 16:54:37 +0000

    Remove developer notes
    Unecessary as I am removing the mentioned behavior

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,9 +6,7 @@ const Home = () => {
   const [newTask, setNewTask] = useState("");
 
   // The array of tasks
-  const [tasks, setTasks] = useState([
-    // "This is a demo task", // Default task can be set here
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   // Adds the user's typed task to the array of tasks
   function addTask() {
@@ -93,19 +91,17 @@ const Home = () => {
           )
         }
       </ul>
-      <h2>Developer notes:</h2>
-      <p><strong>Duplicate tasks will not usually be cleared</strong> when one copy is cleared. My workaround was to add a unique number of spaces to each task (which the browser visually omits*).</p><p>The reason I did this is in case someone intends to complete the same task multiple times. For example, you might type:</p>
-      <ul>
-        <li>Do some work</li>
-        <li>Take a break</li>
-        <li>Do some work</li>
-        <li>Take a break</li>
-      </ul>
-      <p>Without the workaround, when you clear one task, the duplicate is also cleared, making it frustrating if you expected it to only clear one. </p>
-      <p><strong>It might clear duplicates if</strong> someone types accidental spaces at the end of a task, but this is unlikely in practice. It also might clear duplicates with the right combination of adds and clears.</p>
-      <p>Alternatively, I could have added a number to each task, but this would be more complicated to code since the numbers would have to change each time a task is cleared, or otherwise be hidden, which requires more code.</p>
-      <small>*Only one space is shown.</small>
-      <footer><p><strong>Created by Sean Hammond</strong></p><p>Mentors: Alex Ayala and Thomas Brito</p><p>This and many other projects are built by students as part of the 4Geeks Academy Coding Bootcamp by Alejandro Sanchez and many other contributors.</p></footer>
+      <footer>
+        <p>
+          <strong>Created by Sean Hammond</strong>
+        </p>
+        <p>Mentors: Alex Ayala and Thomas Brito</p>
+        <p>
+          This and many other projects are built by students as part of the
+          4Geeks Academy Coding Bootcamp by Alejandro Sanchez and many other
+          contributors.
+        </p>
+      </footer>
     </div>
   );
 };
